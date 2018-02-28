3c2c52dba9f010d6202059ec585e3e279a5159fa
yangwd <yangwd@aibicoin.com>
2018-02-28 19:34:50 +0800

    数据库,redis,utils,常量模块封装

---
+++ dal/DataSource.py
@@ -0,0 +1,18 @@
+# encoding=utf-8
+import sys
+import os
+currentUrl = os.path.dirname(__file__)
+parentUrl = os.path.abspath(os.path.join(currentUrl, os.pardir))
+sys.path.append(parentUrl)
+from datasource.configtools import getconnstring
+from peewee import *
+
+demo = getconnstring('demo')
+demo_database = MySQLDatabase('demo',
+                                        # max_connections=8,
+                                        # stale_timeout=300,
+                                        threadlocals=True, **{'host': demo.host,
+                                                              'password': demo.passwd,
+                                                              'port': demo.port,
+                                                              'user': demo.user,
+                                                              'charset': demo.charset})
