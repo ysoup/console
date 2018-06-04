9115df1ba32409b0e1a83f58c0e24318a8e38fda
rtb <rongtianbiao@aibicoin.com>
2018-06-04 17:54:14 +0800

    '修改'

--- config/celery_config.py
+++ config/celery_config.py
@@ -14,7 +14,7 @@ with open(config_file, "r") as fi:
 CELERY_CREATE_MISSING_QUEUES = True
 
 CELERY_IMPORTS = ("crawler.spider", "crawler.coin_world", "crawler.duplicate_removal", "crawler.eight_btc",
-                  "crawler.bit_coin", "crawler.information_duplicate_removal")
+                  "crawler.bit_coin", "crawler.information_duplicate_removal" ,"crawler.wall_street")
 
 # 使用redis 作为任务队列
 # BROKER_URL = 'redis://:' + REDIS_PASSWORD + '@' + REDIS_HOST + ':' + str(REDIS_PORT) + '/' + str(REDIS_DB_NUM)
@@ -66,8 +66,8 @@ CELERY_QUEUES = (
     Queue('bit_coin_task', exchange=Exchange('bit_coin_task'), routing_key='bit_coin_info'),
     Queue('news_duplicate_removal_task', exchange=Exchange('news_duplicate_removal_task'),
           routing_key='news_duplicate_removal_info'),
-    Queue('wall_street_task', exchange=Exchange('wall_street_task'), routing_key='wall_street_info')
-),Queue('btc_new_flash_task', exchange=Exchange('btc_new_flash_task'), routing_key='btc_new_flash_info'))
+    Queue('wall_street_task', exchange=Exchange('wall_street_task'), routing_key='wall_street_info'),
+    Queue('btc_new_flash_task', exchange=Exchange('btc_new_flash_task'), routing_key='btc_new_flash_info'))
 
 # # 路由
 # CELERY_ROUTES = {
