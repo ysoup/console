9ade4dab0d1a6388cc28b1df21e3cae16c2a8779
yangwd <yangwd@aibicoin.com>
2018-06-26 16:44:07 +0800

    可视化爬虫模板

--- bin/spider_ws.py
+++ bin/spider_ws.py
@@ -1,1 +1,46 @@
 # coding:utf-8
+
+import sys
+import os
+currentUrl = os.path.dirname(__file__)
+parentUrl = os.path.abspath(os.path.join(currentUrl, os.pardir))
+sys.path.append(parentUrl)
+# from common.initRedis import connetcredis
+# from common.untils import *
+from database.spiders_visualization_modle import *
+from common.initlog import Logger
+from common.db_utils import *
+
+logger = Logger(kind="work_path", name="duplicate_removal")
+
+
+# 获取爬虫规则
+def spider_base_rule():
+    print("aaa")
+    base_sql = SpidersVisualizationBase.select().where(SpidersVisualizationBase.is_userful == 1)
+    base_ls = model_to_dicts(base_sql)
+    for x in base_ls:
+        rule_sql = SpidersVisualizationRule.select().where(SpidersVisualizationRule.base_id == x["id"])
+        rule_ls = model_to_dicts(rule_sql)
+        for y in rule_ls:
+            data_handle_sql = SpidersVisualizationDataHandle.select().where(SpidersVisualizationDataHandle.rule == y["id"])
+            data_handle_ls = model_to_dicts(data_handle_sql)
+            y["data_handle"] = data_handle_ls
+        x["crawler_column"] = y
+        yield spiders_template(x)
+
+
+# 动态生成爬虫模板
+def spiders_template(rule_data):
+    print("aaa")
+    # 生成爬虫模板
+#     get_spider_template(rule_data)
+#     # 修改爬虫配置
+#
+#
+# def get_spider_template(rule_data):
+#     print("aaaa")
+
+
+if __name__ == "__main__":
+    spider_base_rule()
---
+++ database/spiders_visualization_modle.py
@@ -0,0 +1,69 @@
+from peewee import *
+import sys
+import os
+currentUrl = os.path.dirname(__file__)
+parentUrl = os.path.abspath(os.path.join(currentUrl, os.pardir))
+sys.path.append(parentUrl)
+from common.initDb import InitDb
+from common.constants import SpidersDataModel
+
+spiders_visualization = InitDb(SpidersDataModel.Model_SPIDERS_VISUALIZATION.value)
+spiders_visualization_database = spiders_visualization.connect()
+spiders_visualization.wirte_logger()
+
+
+class UnknownField(object):
+    def __init__(self, *_, **__): pass
+
+
+class BaseModel(Model):
+    class Meta:
+        database = spiders_visualization_database
+
+
+class SpidersVisualizationBase(BaseModel):
+    fliter_a_tag = IntegerField(null=True)
+    get_num = IntegerField(null=True)
+    html_ls_tag = CharField(null=True)
+    img_watermark = IntegerField(null=True)
+    is_userful = IntegerField(null=True)
+    ls_rule_type = IntegerField(null=True)
+    req_code = CharField(null=True)
+    req_headers = CharField(null=True)
+    req_method = IntegerField(null=True)
+    req_params = CharField(null=True)
+    spider_ch_name = CharField(null=True)
+    spider_en_name = CharField(null=True)
+    target_url = CharField()
+    time_interval = IntegerField(column_name='time interval', null=True)
+
+    class Meta:
+        table_name = 'spiders_visualization_base'
+
+
+class SpidersVisualizationDataHandle(BaseModel):
+    delete_tag = CharField(null=True)
+    end_replace_tag = CharField(null=True)
+    replace_tag = CharField(null=True)
+    rule = IntegerField(column_name='rule_id')
+    type = IntegerField()
+
+    class Meta:
+        table_name = 'spiders_visualization_data_handle'
+        primary_key = False
+
+
+class SpidersVisualizationRule(BaseModel):
+    analysis_code = CharField(null=True)
+    base_id = IntegerField(column_name='base_id')
+    ch_name = CharField(null=True)
+    column_rule_type = IntegerField(null=True)
+    column_type = IntegerField(null=True)
+    en_name = CharField(null=True)
+    get_column_rule = CharField(null=True)
+    get_data_way = IntegerField(null=True)
+    start_for = IntegerField(null=True)
+
+    class Meta:
+        table_name = 'spiders_visualization_rule'
+
